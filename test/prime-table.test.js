35779741c172bba8598c39799c765c5ec9909e24
Fábio Antunes <fabioantuness@gmail.com>
2017-01-22 17:53:00 +0000

    Assert number of calls

--- test/prime-table.test.js
+++ test/prime-table.test.js
@@ -4,13 +4,15 @@ const primeTable = require('../lib/prime-table');
 
 describe('Prime table', () => {
   it('prints prime table to console', done => {
-    const tableSize = 3;
+    const N = 3;
     const spy = sinon.spy(console, 'log');
-    primeTable(tableSize, (err, primes) => {
+    primeTable(N, (err, primes) => {
       assert(spy.calledWith('| | 2| 3| 5|'));
       assert(spy.calledWith('|2| 4| 6| 10|'));
       assert(spy.calledWith('|3| 6| 9| 15|'));
       assert(spy.calledWith('|5| 10| 15|25|'));
+      assert(spy.calledWith('|5| 10| 15|25|'));
+      sinon.assert.callCount(spy, N + 1)
       done(err, primes)
     });
   });
