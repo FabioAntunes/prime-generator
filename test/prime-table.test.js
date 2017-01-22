2dac21cc6f95bab83799b424fc1a7bb1e9e95dd5
Fábio Antunes <fabioantuness@gmail.com>
2017-01-22 18:20:50 +0000

    Fixed test and proper prime-table function

--- test/prime-table.test.js
+++ test/prime-table.test.js
@@ -8,10 +8,9 @@ describe('Prime table', () => {
     const spy = sinon.spy(console, 'log');
     primeTable(N, (err, primes) => {
       assert(spy.calledWith('| | 2| 3| 5|'));
-      assert(spy.calledWith('|2| 4| 6| 10|'));
-      assert(spy.calledWith('|3| 6| 9| 15|'));
-      assert(spy.calledWith('|5| 10| 15|25|'));
-      assert(spy.calledWith('|5| 10| 15|25|'));
+      assert(spy.calledWith('| 2| 4| 6| 10|'));
+      assert(spy.calledWith('| 3| 6| 9| 15|'));
+      assert(spy.calledWith('| 5| 10| 15| 25|'));
       sinon.assert.callCount(spy, N + 1)
       done(err, primes)
     });
